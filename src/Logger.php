e77e8ab4a23be35ed874bd3fba6e3c19d77eb52b
Sam Mousa <sam@mousa.nl>
2022-03-23 17:16:04 +0100

    fix: psr3 compliance and cleaned up the logger using the trait from the psr

--- src/Logger.php
+++ src/Logger.php
@@ -7,125 +7,14 @@
 
 namespace yii\symfonymailer;
 
-use Yii;
+use Psr\Log\InvalidArgumentException;
 use Psr\Log\LoggerInterface;
+use Psr\Log\LoggerTrait;
+use Yii;
 
 final class Logger implements LoggerInterface
 {
-    /**
-     * System is unusable.
-     *
-     * @param string|\Stringable $message
-     * @param mixed[] $context
-     *
-     * @return void
-     */
-    public function emergency($message, array $context = []): void
-    {
-        Yii::getLogger()->log($message, \yii\log\Logger::LEVEL_ERROR, __METHOD__);
-    }
-
-    /**
-     * Action must be taken immediately.
-     *
-     * Example: Entire website down, database unavailable, etc. This should
-     * trigger the SMS alerts and wake you up.
-     *
-     * @param string|\Stringable $message
-     * @param mixed[] $context
-     *
-     * @return void
-     */
-    public function alert($message, array $context = []): void
-    {
-        Yii::getLogger()->log($message, \yii\log\Logger::LEVEL_ERROR, __METHOD__);
-    }
-
-    /**
-     * Critical conditions.
-     *
-     * Example: Application component unavailable, unexpected exception.
-     *
-     * @param string|\Stringable $message
-     * @param mixed[] $context
-     *
-     * @return void
-     */
-    public function critical($message, array $context = []): void
-    {
-        Yii::getLogger()->log($message, \yii\log\Logger::LEVEL_ERROR, __METHOD__);
-    }
-
-    /**
-     * Runtime errors that do not require immediate action but should typically
-     * be logged and monitored.
-     *
-     * @param string|\Stringable $message
-     * @param mixed[] $context
-     *
-     * @return void
-     */
-    public function error($message, array $context = []): void
-    {
-        Yii::getLogger()->log($message, \yii\log\Logger::LEVEL_ERROR, __METHOD__);
-    }
-
-    /**
-     * Exceptional occurrences that are not errors.
-     *
-     * Example: Use of deprecated APIs, poor use of an API, undesirable things
-     * that are not necessarily wrong.
-     *
-     * @param string|\Stringable $message
-     * @param mixed[] $context
-     *
-     * @return void
-     */
-    public function warning($message, array $context = []): void
-    {
-        Yii::getLogger()->log($message, \yii\log\Logger::LEVEL_WARNING, __METHOD__);
-    }
-
-    /**
-     * Normal but significant events.
-     *
-     * @param string|\Stringable $message
-     * @param mixed[] $context
-     *
-     * @return void
-     */
-    public function notice($message, array $context = []): void
-    {
-        Yii::getLogger()->log($message, \yii\log\Logger::LEVEL_WARNING, __METHOD__);
-    }
-
-    /**
-     * Interesting events.
-     *
-     * Example: User logs in, SQL logs.
-     *
-     * @param string|\Stringable $message
-     * @param mixed[] $context
-     *
-     * @return void
-     */
-    public function info($message, array $context = []): void
-    {
-        Yii::getLogger()->log($message, \yii\log\Logger::LEVEL_INFO, __METHOD__);
-    }
-
-    /**
-     * Detailed debug information.
-     *
-     * @param string|\Stringable $message
-     * @param mixed[] $context
-     *
-     * @return void
-     */
-    public function debug($message, array $context = []): void
-    {
-        Yii::getLogger()->log($message, \yii\log\Logger::LEVEL_INFO, __METHOD__);
-    }
+    use LoggerTrait;
 
     /**
      * Logs with an arbitrary level.
@@ -156,7 +45,7 @@ final class Logger implements LoggerInterface
                 $yiiLevel = \yii\log\Logger::LEVEL_INFO;
                 break;
             default:
-                $yiiLevel = \yii\log\Logger::LEVEL_INFO;
+                throw new InvalidArgumentException("Unknown logging level $level");
         }
         Yii::getLogger()->log($message, $yiiLevel, "PSR Logging Adapter");
     }
