5599b517730eaf7a6721ee359d5b2d4c026d1ab1
Sam <sam@mousa.nl>
2022-03-24 14:29:52 +0100

    Update src/SMimeEncrypterWrapper.php
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>

--- src/SMimeEncrypterWrapper.php
+++ src/SMimeEncrypterWrapper.php
@@ -10,7 +10,7 @@ use Symfony\Component\Mime\Message;
 /**
  * @codeCoverageIgnore This class is a trivial proxy that requires no testing
  */
-class SMimeEncrypterWrapper implements SymfonyMessageEncrypterInterface
+class SMimeMessageEncrypter implements SymfonyMessageEncrypterInterface
 {
     private SMimeEncrypter $encrypter;
 
