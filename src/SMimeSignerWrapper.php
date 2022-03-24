f6a284e6cbc950c0e33267db0e22d79eafaea555
Sam <sam@mousa.nl>
2022-03-24 14:30:18 +0100

    Update src/SMimeSignerWrapper.php
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>

--- src/SMimeSignerWrapper.php
+++ src/SMimeSignerWrapper.php
@@ -10,7 +10,7 @@ use Symfony\Component\Mime\Message;
 /**
  * @codeCoverageIgnore This class is a trivial proxy that requires no testing
  */
-class SMimeSignerWrapper implements SymfonyMessageSignerInterface
+class SMimeMessageSigner implements SymfonyMessageSignerInterface
 {
     private SMimeSigner $signer;
 
